bc1e9a9da700518436abb8497b28a326bb241121
minzi03 <minhduy200316@gmail.com>
2023-11-23 00:21:49 +0700

    update

---
+++ Models/GroupUser.cs
@@ -0,0 +1,30 @@
+﻿namespace DoAn.Models
+{
+    public class GroupUser
+    {
+        String id {  get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String UserId { get; set; }
+        public String UserName { get; set; }
+        public String UserEmail { get; set; }
+        public String UserPassword { get; set; }
+        
+        GroupUser()
+        {
+
+        }
+        GroupUser(string id, string name, string email, string password, string userId, string userName, string userEmail, string userPassword)
+        {
+            this.id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            UserId = userId;
+            UserName = userName;
+            UserEmail = userEmail;
+            UserPassword = userPassword;
+        }
+    }
+}
