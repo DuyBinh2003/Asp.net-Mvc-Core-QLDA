d2cc4383e655fef1caa06d4f1a682ee9754bf407
minzi03 <minhduy200316@gmail.com>
2023-12-19 19:31:14 +0700

    hoan thanh admin

--- Areas/Admin/Controllers/BooksController.cs
+++ Areas/Admin/Controllers/BooksController.cs
@@ -46,17 +46,13 @@ namespace DoAn.Areas.Admin.Controllers
             }
 
             var book = await _context.Books
-                 .Include(s => s.InvoiceDetails)
-                 .Include(s => s.Author)
-                 //.Include(s => s.InvoiceDetails)
-                 //   .ThenInclude(i => i.Invoice)
-                 .Include(s => s.Carts)
-                    .ThenInclude(u => u.User)
-
-                 //.Include(s => s.Carts)
-                 //   .ThenInclude(u => u.Invoice)
-                 .Include(s => s.Category)
+                .Include(s => s.InvoiceDetails)
+                    .ThenInclude(invoiceDetail => invoiceDetail.Invoice)
+                        .ThenInclude(i => i.User)
+                .Include(s => s.Author) 
+                .Include(s => s.Category)
                 .FirstOrDefaultAsync(m => m.BookId == id);
+
             if (book == null)
             {
                 return NotFound();
